4a05c1039ff2a25b823c8abb66115363be03a56e
wongx93 <67490859+wongx93@users.noreply.github.com>
2021-01-21 19:49:49 -0500

    dfadfasf

--- script.js
+++ script.js
@@ -99,14 +99,7 @@ $(window).scroll(function() {
 $(window).scroll(function() {
     var scrollDistance = $(window).scrollTop();
   
-    // Show/hide menu on scroll
-    //if (scrollDistance >= 850) {
-    //		$('nav').fadeIn("fast");
-    //} else {
-    //		$('nav').fadeOut("fast");
-    //}
-  
-    // Assign active class to nav links while scolling
+
     $('.page-section').each(function(i) {
         if ($(this).position().top <= scrollDistance) {
             $('.sidebar2 a.active').removeClass('active');
