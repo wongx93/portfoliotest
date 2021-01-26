28dab592a9f9fff9fe68d8858039371bb1fbcdf3
wongx93 <67490859+wongx93@users.noreply.github.com>
2021-01-25 22:56:31 -0500

    dafsdfasdf

--- script.js
+++ script.js
@@ -50,11 +50,10 @@ window.onload = showSlides;
 
 $(document).ready(function() {
   $('a[href*=#]').bind('click', function(e) {
-      e.preventDefault(); // prevent hard jump, the default behavior
+      e.preventDefault(); 
+      var target = $(this).attr("href"); 
 
-      var target = $(this).attr("href"); // Set the target as variable
-
-      // perform animated scrolling by getting top-position of target-element and set it as scroll target
+  
       $('html, body').stop().animate({
           scrollTop: $(target).offset().top
       }, 600, function() {
